06b8c43ab65b2c3a8c3fa7084fd996c67d76f7f6
Heze <git@mionch.net>
2023-12-06 18:59:36 +0300

    Line utility functions refactor

---
+++ python/line_utils.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+def intersection_point_polar(line_a, line_b):
+    '''
+    Calculates an intersection point between two lines.
+    
+    Parameters:
+    line_a: (float, float)
+        Polar coordinates of the first line. Must contain distance from origin
+        at index 0 and angle in radians at index 1.
+    line_b: (float, float)
+        Polar coordinates of the second line. Must contain distance from origin
+        at index 0 and angle in radians at index 1.
+    
+    Returns:
+    (1, (int, int))
+        Status 1 and tuple, containing the coordinates of the intersection
+        point if possible.
+    (0, (0, 0))
+        Status 0 and a (0, 0) tuple if the lines are parallel.
+    '''
+    distance_a, distance_b = line_a[0], line_b[0]
+    angle_a, angle_b = line_a[1], line_b[1]
+    denominator = np.sin(angle_a - angle_b)
+    if denominator != 0:
+        point_x = distance_b * np.sin(angle_a) - distance_a * np.sin(angle_b)
+        point_x = int(point_x / denominator)
+        point_y = distance_a * np.cos(angle_b) - distance_b * np.cos(angle_a)
+        point_y = int(point_y / denominator)
+        return (1, (point_x, point_y))
+    return (0, (0, 0))
+
+def vertical_intercepts_polar(line, limit):
+    '''
+    Calculates points at which given line intersects y = 0 and y = limit.
+
+    Parameters:
+    line: (float, float)
+        Polar coordinates of the line. Must contain distance from origin at
+        index 0 and angle in radians at index 1.
+    limit: float
+        Y coordinate of the second line.
+    
+    Returns:
+    (1, (int, int), (int, int))
+        Status 1 and coordinates of two intersection points if possible.
+    (0, (0, 0), (0, 0))
+        Status 0 and two (0, 0) tuples if given line is parallel to the y = 0.
+    '''
+    point_1 = intersection_point_polar(line, (0, np.pi / 2))
+    point_2 = intersection_point_polar(line, (limit, np.pi / 2))
+    if point_1[0] and point_2[0]: 
+        return (1, point_1[1], point_2[1])
+    return (0, (0, 0), (0, 0))
+
+def horizontal_intercepts_polar(line, limit):
+    '''
+    Calculates points at which given line intersects x = 0 and x = limit.
+
+    Parameters:
+    line: (float, float)
+        Polar coordinates of the line. Must contain distance from origin at
+        index 0 and angle in radians at index 1.
+    limit: float
+        X coordinate of the second line.
+    
+    Returns:
+    (1, (int, int), (int, int))
+        Status 1 and coordinates of two intersection points if possible.
+    (0, (0, 0), (0, 0))
+        Status 0 and two (0, 0) tuples if given line is parallel to the x = 0.
+    '''
+    point_1 = intersection_point_polar(line, (0, 0))
+    point_2 = intersection_point_polar(line, (limit, 0))
+    if point_1[0] and point_2[0]: 
+        return (1, point_1[1], point_2[1])
+    return (0, (0, 0), (0, 0))
+
+def lines_weighted_average(line_a, line_b, threshold):
+    '''
+    Attempts to calculate a weighted average of the two lines by taking pairs
+    of their points and then finding a weighted average point for each pair.
+
+    Parameters:
+    line_a: (float, (float, float), (float, float))
+        Weight of the first line followed by coordinates of it's two points.
+    line_b: (float, (float, float), (float, float))
+        Weight of the second line followed by coordinates of it's two points.
+    threshold: float
+        Maximum distance between points in a pair until which finding an
+        average line will be attempted.
+    
+    Returns:
+    (1, float, (int, int), (int, int))
+        Status 1, new line weight and tuple, containing the coordinates of the 
+        two average points that define an average line if possible.
+    (0, 0, (0, 0), (0, 0))
+        Status 0, weight 0 and two (0, 0) tuples if average could not be found.
+    '''
+    delta_1 = (((line_a[1][0] - line_b[1][0]) ** 2) + 
+               ((line_a[1][1] - line_b[1][1]) ** 2)) ** 0.5
+    delta_2 = (((line_a[2][0] - line_b[2][0]) ** 2) +
+               ((line_a[2][1] - line_b[2][1]) ** 2)) ** 0.5
+    if delta_1 < threshold and delta_2 < threshold:
+        weight = line_a[0] + line_b[0]
+        x1 = (line_a[1][0] * line_a[0] + line_b[1][0] * line_b[0]) / weight
+        y1 = (line_a[1][1] * line_a[0] + line_b[1][1] * line_b[0]) / weight
+        x2 = (line_a[2][0] * line_a[0] + line_b[2][0] * line_b[0]) / weight
+        y2 = (line_a[2][1] * line_a[0] + line_b[2][1] * line_b[0]) / weight
+        return (1, weight, (int(x1), int(y1)), (int(x2), int(y2)))
+    return (0, 0, (0, 0), (0, 0))
--- python/recognition.py
+++ python/recognition.py
@@ -1,26 +1,6 @@
 import cv2
 import numpy as np
-
-def transform_line(distance, angle, image_size):
-    '''
-    TODO: add info
-    '''
-    if abs(np.cos(angle)) < 0.1:
-        intercept_max_x = image_size[1]
-        intercept_max_y = (-np.cos(angle) / np.sin(angle)) * image_size[1]
-        intercept_max_y += distance / np.sin(angle)
-        intercept_min = (0, int(distance / np.sin(angle)))
-        intercept_max = (int(intercept_max_x), int(intercept_max_y))
-        return (1, intercept_min, intercept_max)
-    elif abs(np.sin(angle)) < 0.1:
-        intercept_max_y = image_size[0]
-        intercept_max_x = image_size[0] * np.sin(angle) - distance
-        intercept_max_x /= -np.cos(angle)
-        intercept_min = (int(distance / np.cos(angle)), 0)
-        intercept_max = (int(intercept_max_x), int(intercept_max_y))
-        return (2, intercept_min, intercept_max)
-    else:
-        return (0, 0, 0)
+import line_utils as lu
 
 def find_lines(source, display=False, **params):
     '''
@@ -56,62 +36,44 @@ def find_lines(source, display=False, **params):
     delta = params.get('delta', image_original.shape[0] / 18)
     for line in lines:
         for distance, angle in line:
-            new_line = transform_line(distance, angle, image_original.shape)
-            if new_line[0] == 0:
-                continue
-            elif new_line[0] == 1:
+            if abs(np.cos(angle)) < 0.1:
+                intercepts = lu.horizontal_intercepts_polar(
+                    (distance, angle), image_original.shape[1])
                 similar = False
                 for line_i in range(len(lines_horizontal)):
-                    intercept_min = lines_horizontal[line_i][0][1]
-                    intercept_max = lines_horizontal[line_i][1][1]
-                    weight = lines_horizontal[line_i][2]
-                    delta_min = abs(intercept_min - new_line[1][1])
-                    delta_max = abs(intercept_max - new_line[2][1])
-                    if (delta_min < delta and delta_max < delta):
-                        x1 = int((lines_horizontal[line_i][0][0] * weight + 
-                                  new_line[1][0]) / (weight + 1))
-                        y1 = int((lines_horizontal[line_i][0][1] * weight + 
-                                  new_line[1][1]) / (weight + 1))
-                        x2 = int((lines_horizontal[line_i][1][0] * weight + 
-                                  new_line[2][0]) / (weight + 1))
-                        y2 = int((lines_horizontal[line_i][1][1] * weight + 
-                                  new_line[2][1]) / (weight + 1))
-                        lines_horizontal[line_i] = [(x1, y1), (x2, y2), 
-                                                    weight + 1]
+                    average = lu.lines_weighted_average(
+                        lines_horizontal[line_i],
+                        (1, intercepts[1], intercepts[2]), delta)
+                    if average[0]:
+                        lines_horizontal[line_i] = (average[1], average[2],
+                            average[3])
                         similar = True
                         break
                 if not similar:
-                    lines_horizontal.append([new_line[1], new_line[2], 1])
-            else:
+                    lines_horizontal.append((1, intercepts[1], intercepts[2]))
+            elif abs(np.sin(angle)) < 0.1:
+                intercepts = lu.vertical_intercepts_polar(
+                    (distance, angle), image_original.shape[0])
                 similar = False
                 for line_i in range(len(lines_vertical)):
-                    intercept_min = lines_vertical[line_i][0][0]
-                    intercept_max = lines_vertical[line_i][1][0]
-                    weight = lines_vertical[line_i][2]
-                    delta_min = abs(intercept_min - new_line[1][0])
-                    delta_max = abs(intercept_max - new_line[2][0])
-                    if (delta_min < delta and delta_max < delta):
-                        x1 = int((lines_vertical[line_i][0][0] * weight + 
-                                  new_line[1][0]) / (weight + 1))
-                        y1 = int((lines_vertical[line_i][0][1] * weight + 
-                                  new_line[1][1]) / (weight + 1))
-                        x2 = int((lines_vertical[line_i][1][0] * weight + 
-                                  new_line[2][0]) / (weight + 1))
-                        y2 = int((lines_vertical[line_i][1][1] * weight + 
-                                  new_line[2][1]) / (weight + 1))
-                        lines_vertical[line_i] = [(x1, y1), (x2, y2), 
-                                                  weight + 1]
+                    average = lu.lines_weighted_average(
+                        lines_vertical[line_i],
+                        (1, intercepts[1], intercepts[2]), delta)
+                    if average[0]:
+                        lines_vertical[line_i] = (average[1], average[2],
+                            average[3])
                         similar = True
                         break
                 if not similar:
-                    lines_vertical.append([new_line[1], new_line[2], 1])
+                    lines_vertical.append((1, intercepts[1], intercepts[2]))
 
     if (display):
+        print(len(lines_horizontal), len(lines_vertical))
         image_lines = np.copy(image_original) * 0
         lines_color = params.get('color', (255, 0, 0))
-        for point1, point2, _ in lines_horizontal:
+        for _, point1, point2 in lines_horizontal:
             cv2.line(image_lines, point1, point2, lines_color, 5)
-        for point1, point2, _ in lines_vertical:
+        for _, point1, point2 in lines_vertical:
             cv2.line(image_lines, point1, point2, lines_color, 5)
         image_display = cv2.addWeighted(image_original, 0.8, image_lines, 1, 0)
         cv2.imshow("Detected lines", image_display)
